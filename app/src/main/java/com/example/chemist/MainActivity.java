cf4d47f18f87a1bec7a1940b26f0eb6b36bae59f
Joy-Ndirangu <joyndirangu18@gmail.com>
2024-02-17 20:58:20 +0300

    Added more material design components to the layouts

--- app/src/main/java/com/example/chemist/MainActivity.java
+++ app/src/main/java/com/example/chemist/MainActivity.java
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().setTitle("Maarjan");
+
 //        Button start_btn = (Button) findViewById(R.id.startbtn);
 //        start_btn.setOnClickListener(new View.OnClickListener() {
 //            public void onClick(View v) {
--- app/src/main/java/com/example/chemist/manager_home.java
+++ app/src/main/java/com/example/chemist/manager_home.java
@@ -3,12 +3,48 @@ package com.example.chemist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toolbar;
 
 public class manager_home extends AppCompatActivity {
+
+    Toolbar topAppBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manager_home);
+
+
+        topAppBar = findViewById(R.id.topAppBar);
+
+        topAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle navigation icon press
+            }
+        });
+
+        topAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.profilepic:
+                        // Handle favorite icon press
+                        return true;
+                    case R.id.search:
+                        // Handle search icon press
+                        return true;
+                    case R.id.more:
+                        // Handle more item (inside overflow menu) press
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+
     }
 }
