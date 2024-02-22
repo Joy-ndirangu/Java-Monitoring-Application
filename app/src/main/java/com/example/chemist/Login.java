44819861c3a925764ffe5a450b7a2fe37308e4d2
Joy-Ndirangu <joyndirangu18@gmail.com>
2024-02-22 19:21:05 +0300

    fetching user data from db

--- app/src/main/java/com/example/chemist/Login.java
+++ app/src/main/java/com/example/chemist/Login.java
@@ -1,5 +1,6 @@
 package com.example.chemist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,31 +8,142 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Login extends AppCompatActivity {
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
-    Button login_btn, empbtn;
+public class Login extends AppCompatActivity {
+    //variables
+    TextInputLayout username, password;
+
+    Button noaccount_btn, login_btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Button login_btn = (Button) findViewById(R.id.noaccount);
-        login_btn.setOnClickListener(new View.OnClickListener() {
+        username = findViewById(R.id.usernamelogin);
+        password = findViewById(R.id.passwordlogin);
+        noaccount_btn = findViewById(R.id.noaccount);
+        login_btn = findViewById(R.id.loginbtn);
+
+        noaccount_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent;
-                intent = new Intent(Login.this,SignUp.class);
+                intent = new Intent(Login.this, SignUp.class);
                 startActivity(intent);
 
             }
         });
 
-        Button empbtn = (Button) findViewById(R.id.loginbtn);
-        empbtn.setOnClickListener(new View.OnClickListener() {
+        login_btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent;
-                intent = new Intent(Login.this, manager_home.class);
-                startActivity(intent);
+
+                //Validate Login Info
+                if (!validateUsername() | !validatePassword()) {
+                    username.setError("User not authenticated");
+                } else {
+                    //to check if there is data related to the username and password in firebase
+                    isUser();
+                }
+
+            }
+        });
+    }
+
+
+    //validation of username and password
+
+    //For username
+    private Boolean validateUsername() {
+        String val = username.getEditText().getText().toString();
+        if (val.isEmpty()) {
+            username.setError("Field cannot be empty");
+            return false;
+        } else {
+            username.setError(null);
+            username.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    //For password
+    private Boolean validatePassword() {
+        String val = password.getEditText().getText().toString();
+        if (val.isEmpty()) {
+            password.setError("Field cannot be empty");
+            return false;
+        } else {
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+
+    //get the user entered values Username and Password in String using the isUser()method
+    private void isUser() {
+//        progressBar.setVisibility(View.VISIBLE);
+
+
+        //getting the user entered details/values inside a string
+        final String userEnteredUsername = username.getEditText().getText().toString().trim();
+        final String userEnteredPassword = password.getEditText().getText().toString().trim();
+
+        //to start using the data stored in the firebase realtime database we need a database reference variable
+        //instance is the parent node of the data tree in our firebase
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+
+        //query looks inside the username for all the users and match it with the value entered by the user.
+        Query checkUser = reference.orderByChild("username").equalTo(userEnteredUsername);
+        checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //checking if there is data inside the datasnapshot
+                if (dataSnapshot.exists()) {
+                    username.setError(null);
+                    username.setErrorEnabled(false);
+                    //if the data exists, fetch the password of that user and get it inside this string
+                    String passwordFromDB = dataSnapshot.child(userEnteredUsername).child("password").getValue(String.class);
+
+                    //checking if the password in database is similar to that entered(authentication)
+                    if (passwordFromDB.equals(userEnteredPassword)) {
+                        username.setError(null);
+                        username.setErrorEnabled(false);
+                        String nameFromDB = dataSnapshot.child(userEnteredUsername).child("name").getValue(String.class);
+                        String usernameFromDB = dataSnapshot.child(userEnteredUsername).child("username").getValue(String.class);
+                        String phoneNoFromDB = dataSnapshot.child(userEnteredUsername).child("phone").getValue(String.class);
+                        String emailFromDB = dataSnapshot.child(userEnteredUsername).child("email").getValue(String.class);
+
+                        Intent intent = new Intent(Login.this, UserProfile.class);
+                        //initializing data inside the intent
+                        intent.putExtra("name", nameFromDB);
+                        intent.putExtra("username", usernameFromDB);
+                        intent.putExtra("email", emailFromDB);
+                        intent.putExtra("phone", phoneNoFromDB);
+                        intent.putExtra("password", passwordFromDB);
+                        startActivity(intent);
+                    } else {
+//                        progressBar.setVisibility(View.GONE);
+                        password.setError("Wrong Password");
+                        password.requestFocus();
+                    }
+                } else {
+//                    progressBar.setVisibility(View.GONE);
+                    username.setError("No such User exist");
+                    username.requestFocus();
+                }
+            }
+
+            //empty implementation for the onCancelled() method since i don't need to handle any specific logic when a database operation is canceled.
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
 
             }
         });
--- app/src/main/java/com/example/chemist/UserProfile.java
+++ app/src/main/java/com/example/chemist/UserProfile.java
@@ -2,13 +2,50 @@ package com.example.chemist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class UserProfile extends AppCompatActivity {
+
+    //variables
+    TextInputLayout profile_name, profile_username, profile_email, profile_phone, profile_password;
+    TextView fullNameLabel, usernameLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+//hooks
+        profile_name = findViewById(R.id.profile_fullname);
+        profile_username = findViewById(R.id.profile_username);
+        profile_email = findViewById(R.id.profile_email);
+        profile_phone = findViewById(R.id.profile_phone);
+        profile_password = findViewById(R.id.profile_password);
+        fullNameLabel = findViewById(R.id.fullname_label);
+        usernameLabel = findViewById(R.id.username_label);
+
+        //function to show all data
+        showAllUserData();
+
+
+    }
+
+    private void showAllUserData() {
+        Intent intent = getIntent();
+        String user_username = intent.getStringExtra("username");
+        String user_name = intent.getStringExtra("name");
+        String user_email = intent.getStringExtra("email");
+        String user_phoneNo = intent.getStringExtra("phone");
+        String user_password = intent.getStringExtra("password");
+
+        fullNameLabel.setText(user_name);
+        usernameLabel.setText(user_username);
+        profile_name.getEditText().setText(user_name);
+        profile_email.getEditText().setText(user_email);
+        profile_phone.getEditText().setText(user_phoneNo);
+        profile_password.getEditText().setText(user_password);
     }
 }
